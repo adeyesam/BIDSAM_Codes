63fd00950e0b65b40a5b178c6025a981cab22a81
Samuel Adeyemo <148823677+adeyesam@users.noreply.github.com>
2023-11-02 18:06:15 -0400

    Add files via upload

---
+++ BIDSAM_V0/Dynamic BML Model/simulate_model_dynamic.m
@@ -0,0 +1,112 @@
+%% The file loads and simulates hierarchically ranked models (based on AICc+2Cve values)
+% from the branch and bound algorithm. 'solnrank' is the rank of model we desire to simulate.
+% The rows of 'inputs' are the input variables while the columns are the time instants
+
+
+function y = simulate_model_dynamic(inputs, y_initial, solnrank)
+
+load('solution_workspace')
+u = inputs;
+[lengthu, lengthu2] = size(u);
+N2 = N^2;
+
+% Scaling inputs and initial outputs
+for i = 1:lengthu
+    u(i,:) = 1+2*(u(i,:)-umin(i))/(umax(i)-umin(i));
+end
+for i=1:N
+    y_initial(i,:)=1 + 2*(y_initial(i,:)-mnd(i))/(mxd(i)-mnd(i));
+end
+
+XXQ = y_initial;
+XQ(:,1) = XXQ;
+
+
+for jj=1:lengthu2-1
+    
+    % Compute input transformations
+    if lengthu>1
+        i_UiUj = nchoosek(1:lengthu,2);                     % Indices for Mixed second order inputs
+        UiUj = zeros(size(i_UiUj,1),1);
+        for i = 1:size(i_UiUj,1)
+                UiUj(i,:) = u(i_UiUj(i,1),jj) .* u(i_UiUj(i,2),jj);
+        end
+    end
+    n_uy = max(N,lengthu);
+    
+    mii=[];
+    for i=1:min(N,lengthu)
+        mii=[mii;[i i]];
+    end
+    iUY = nchoosek(1:n_uy,2);                            % Combination of indices if n(u)=n(measured outputs)=n_uy
+    iUY=[iUY;flip(iUY,2)];
+    [~,iUY1]=sort(iUY(:,1));
+    iUY=[mii;iUY(iUY1,:)];
+    if n_uy==N                                          % Eliminate indices aloted to u greater than n(u)
+        elim = find(iUY(:,1)>lengthu);
+        iUY(elim,:) = [];
+    else                                                % Eliminate indices alotted to y greater than n(measured outputs)
+        elim = find(iUY(:,2)>N);
+        iUY(elim,:) = [];
+    end
+    UY = zeros(size(iUY,1),1);
+    for i=1:size(iUY,1)
+        UY(i,:) = u(iUY(i,1),jj).*XXQ(iUY(i,2),:);     % Compute U*Y
+    end
+    U2 = u(:,jj) .* u(:,jj);
+    LogU = log(u(:,jj));                                % Natural log transformation
+    ExpU = exp(-u(:,jj));                               % Exponential transformation
+    InvU = 1./u(:,jj);                                  % Inverse of inputs
+    InvUsq = (u(:,jj).*u(:,jj)).\1;                     % Inverse of squared inputs
+    sqrtU = sqrt(u(:,jj));                              % Square root of inputs
+    sqrtInvU = sqrtU.\1;                                % 1/sqrt(u)
+    sigmoidU = (1+exp(-u(:,jj))).\1;                    % Sigmoid
+
+    if lengthu>1
+        Utrans = [ones(1,1); u(:,jj); UY; UiUj; U2; LogU; ExpU; InvU; InvUsq; sqrtU; sqrtInvU; sigmoidU];
+    else
+        Utrans = [ones(1,1); u(:,jj); UY; U2; LogU; ExpU; InvU; InvUsq; sqrtU; sqrtInvU; sigmoidU];
+    end
+    Ntrans = size(Utrans,1);                            % NUMBER OF TRANSFORMATIONs
+    
+    % Arranging Utrans according to rank
+    Utrans_r = Utrans(idOb,:);                          
+
+    if Ntrans>85
+        Ntrans = 85;
+        Utrans_r = Utrans_r(1:Ntrans,:);
+    end
+N=4;
+    % Model parameters for particular rank
+    sln = solnrank;
+    indx = find(Obtrnd(sln,:));
+    Cm = Cmat(sln,:);                 
+    Am = Amat(sln,:);  
+    Cm_ = reshape(Cm,Ntrans,N)';      
+    A = reshape(Am,N,N)';
+    C = Cm_(:,indx);      
+    
+    % Model simulation
+    Utrans_ = Utrans_r(indx,:);                         % Selecting relevant input transformations
+    if jj==1
+        XQ(:,jj+1) = A*XXQ + C*Utrans_;
+    else
+        XQ(:,jj+1) = A*XQ(:,jj) + C*Utrans_;
+    end
+        
+    XXQ = XQ(:,jj+1);
+
+    for j=1:size(XQ,1)
+        y(j,jj) = mnd(j) + 0.5*(XQ(j,jj)-1)*(mxd(j)-mnd(j));
+    end
+end
+
+% Model interpretation
+fprintf(['\nThe identified model is of the form \n \ny(k+1) = A*y(k) + C*Utrans(k)' ...
+    ' \n \nBelow are the model parameters and the selected basis functions in Utrans \n \n']);
+A
+C
+BasisInterpretation(lengthu,iUY,i_UiUj,Obtrnd,idOb,sln,true);
+
+return
+
---
+++ BIDSAM_V0/Steady State BML Model/Run_main.m
@@ -0,0 +1,121 @@
+%% Load data 
+% load('data.mat');
+% load('t.mat');
+% load('u.mat');
+
+% The rows of 'data' and 'u' are the measured output and input variables
+% respectively while the columns are the time instants. 
+
+%% Dimension of data
+N = size(data,1);                                       % No. of output variablees
+N2 = N^2;                       
+[lengthu, lengthu2] = size(u);                          % Dimension of input data
+lenreading = size(data,2)-1;
+lenreadpone = lenreading+1;
+
+%% Range of input/output variables
+umax=zeros(1,lengthu); umin=umax;
+for i=1:lengthu
+    umax(i) = max(u(i,:)) + 0.15*abs(max(u(i,:)));
+    umin(i) = min(u(i,:)) - 0.15*abs(min(u(i,:)));
+end
+for i=1:size(data,1)
+    mxd(i)=max(data(i,:)) + 0.15*abs(max(data(i,:)));
+    mnd(i)=min(data(i,:)) - 0.15*abs(min(data(i,:)));
+end
+
+
+%% Normalization of input/output data
+for i=1:size(data,1)
+    data(i,:)=1 + 2*(data(i,:)-mnd(i))/(mxd(i)-mnd(i));
+end
+for i = 1:size(u,1)
+    u(i,:) = 1+2*(u(i,:)-umin(i))/(umax(i)-umin(i));
+end
+
+%% Compute input transformations
+[Utrans, i_UiUj] = InputTransformation(data,u,[],true);
+Ntrans = size(Utrans,1)                                     % No. of transformations
+BasisInterpretation(lengthu,i_UiUj,[],[],[],false);
+
+%% Selecting specific data points for ranking and estimation
+rank_sel = randsample(lenreadpone,floor(0.3*lenreadpone));
+est_sel = randsample(lenreadpone,floor(0.7*lenreadpone));
+
+data_rank=data(:,rank_sel);
+u_rank=u(:,rank_sel);
+utrans_rank=Utrans(:,rank_sel);
+
+data_est=data(:,est_sel);
+u_est=u(:,est_sel);
+utrans_est=Utrans(:,est_sel);
+
+%% Ranking basis functions
+save('data4rank','data_rank','u_rank','utrans_rank')
+clear data_rank u_rank utrans_rank
+
+gh=zeros(1,Ntrans,Ntrans);
+for i=1:Ntrans
+    gh(:,i,i)=1;
+end
+
+ind=zeros(1,Ntrans);
+for i=1:Ntrans
+    ind=gh(:,:,i); i
+    [Ob(i,:), Cm(i,:)] = RankBasis(ind);
+end
+[Ob, idOb]=sort(Ob);
+Utrans=Utrans(idOb,:);
+
+%%
+utrans_est = utrans_est(idOb,:);
+if Ntrans>85
+    nut = 85;                                               % No. of top rank Utrnas to be considered for BnB
+    Ntrans = nut;
+    utrans_est = utrans_est(1:nut,:);
+else
+    nut = Ntrans;
+end
+
+save('data4est','data_est','u_est','utrans_est','idOb','nut')
+clear data_est u_est utrans_est 
+
+%% Branch and Bound for model selection
+ndes = 8;                                                   % Number of desired hierarchically ranked models
+Nworker = 8;                                                % Number of workers
+[AICcve, Cmat, Lmat, Obtrnd] = BranchNBound(N,ndes,Ntrans,Nworker)
+
+figure
+plot(nonzeros(Obtrnd(1,:)))
+ylabel('Objective fcn (AICc)')
+xlabel('Number of Basis fcns')
+
+%% Save solution workspace
+save('solution_workspace','N','AICcve','Cmat','est_sel','idOb','Lmat','mnd','mxd','umax','umin','Ntrans','Obtrnd','i_UiUj')
+
+%% Post processing of indices of transformed inputs for interpretation
+% slnrank = 1;                                                % Rank of model to be interpreted
+% BasisInterpretation(lengthu,i_UiUj,Obtrnd,idOb,slnrank,true);
+
+%% Simulate model 1 and display training plots
+slnrank = 1;
+load('data4est.mat')
+for i=1:N 
+    data_est(i,:) = mnd(i) + 0.5*(data_est(i,:)-1)*(mxd(i)-mnd(i));
+end
+for i=1:lengthu 
+    u_est(i,:) = umin(i) + 0.5*(u_est(i,:)-1)*(umax(i)-umin(i));
+end
+y = simulate_model_steady(u_est, slnrank);
+
+for k=1:N
+    figure
+    plot(y(k,:),'+','Color',"#77AC30")
+    hold on
+    plot(data_est(k,:),'o','Color',"#D95319")
+    hold off
+    legend('BIDSAM model','Data')
+    title(['Training plot for variable y', num2str(k)])
+    xlabel('indices')
+    ylabel(['y',num2str(k)])
+end
